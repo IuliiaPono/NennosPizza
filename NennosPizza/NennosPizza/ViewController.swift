65ddcb24418fd69989d82ab05d94a05ff832f3c0
Iuliia Ponomareva <i.ponomareva@andersenlab.com>
2019-09-09 15:26:11 +0300

    initial commit

---
+++ NennosPizza/NennosPizza/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NennosPizza
+//
+//  Created by Iuliia Ponomareva on 09/09/2019.
+//  Copyright © 2019 Iuliia Ponomareva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
